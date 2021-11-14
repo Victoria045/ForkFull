a13b79c5f2a721e44576af9e55ff4e97cb989578
Victoria045 <victoriaberyl45@gmail.com>
2021-11-14 21:00:49 +0300

    import werkzeug file storage

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_uploads import UploadSet, configure_uploads, IMAGES 
+from werkzeug.utils import secure_filename
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
