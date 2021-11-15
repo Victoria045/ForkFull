5e32b990a3a4f00fa22a01ee44ec6b5b8697b088
JoyWambui <joywambuindegwa@gmail.com>
2021-11-15 15:26:44 +0300

    View a single upload
    Pulled from Victoria's changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 from flask import Flask,render_template,request,redirect,url_for
+=======
+from distutils.command.upload import upload
+from flask import Flask,render_template,request,redirect,url_for,abort
+>>>>>>> 89d5507 (View a single upload)
 from . import main 
 from .. import db,photos 
 from .forms import UploadForm
@@ -40,6 +45,39 @@ def new_upload():
         # import pdb; pdb.set_trace()
         return redirect(url_for('main.index')) 
 
+<<<<<<< HEAD
     # else:
     #     file_url = None
     return render_template('upload.html', form=form) 
+=======
+    return render_template('upload.html', form=form)
+
+@main.route('/uploads/<int:id>')
+#@login_required
+def uploads(id):
+    upload = Upload.query.get(id)
+    return render_template('single_upload.html',upload=upload)
+
+@main.route('/uploads/<int:id>/update', methods=['GET', 'POST'])
+#@login_required
+def update_upload(id):
+    upload = Upload.query.get(id)
+    if upload is None:
+        abort(404)
+    # if upload.user != current_user:
+    #     abort(403)
+    form = UploadForm() 
+    if form.validate_on_submit():
+        # image = form.image.data 
+        upload.name = form.name.data 
+        upload.category = form.category.data 
+        upload.price = form.price.data 
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        form.name.data=upload.name
+        form.category.data = upload.category
+        form.price.data = upload.price
+    title='Update Upload'
+    return render_template('upload.html', title=title,form=form)
+>>>>>>> 89d5507 (View a single upload)
