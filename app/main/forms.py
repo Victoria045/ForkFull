ed876142105dd4f475de11ea36af743ed75ac0ec
Victoria045 <victoriaberyl45@gmail.com>
2021-11-15 14:40:44 +0300

    crate the UploadForm entities

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed 
 from wtforms import StringField,IntegerField,SelectField,TextAreaField,SubmitField 
-from wtforms.validators import Required 
+from wtforms.validators import DataRequired 
 
 class UploadForm(FlaskForm):
-    picture = FileField('Select picture upload', validators=[FileAllowed('jpg','png')])
-    name = StringField('Enter Restaurant Name', validators=[Required()]) 
-    category = SelectField('Category', choices=[('BreakFast','BreakFast'), ('Lunch','Lunch'),('Diner','Diner')])
-    price = IntegerField('Price', validators=[Required()]) 
+    # image_path = FileField('Select picture upload', validators=[FileAllowed('jpg','png')])
+    name = StringField('Enter Food Name', validators=[DataRequired()]) 
+    category = SelectField('Category', choices=[('BreakFast','BreakFast'), ('Lunch','Lunch'),('Dinner','Dinner')])
+    price = IntegerField('Price', validators=[DataRequired()]) 
     submit = SubmitField('Upload')
