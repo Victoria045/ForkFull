6d2111617c3ecfe1eced9ed37a994a7df972edfe
Victoria045 <victoriaberyl45@gmail.com>
2021-11-18 07:40:45 +0300

    upgrade db

--- migrations/versions/0d5c5d2d511f_initial_migration.py
+++ migrations/versions/0d5c5d2d511f_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial Migration
 
-Revision ID: 84df002e171e
+Revision ID: 0d5c5d2d511f
 Revises: 
-Create Date: 2021-11-16 15:07:38.755384
+Create Date: 2021-11-17 20:16:23.608611
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '84df002e171e'
+revision = '0d5c5d2d511f'
 down_revision = None
 branch_labels = None
 depends_on = None
