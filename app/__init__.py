fabc28a3c94664515781e143e0296e113101d44e
Victoria045 <victoriaberyl45@gmail.com>
2021-11-18 22:06:01 +0300

    modify app __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ mail = Mail()
 def create_app(config_name):
 
     app = Flask(__name__)
-~
+
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
     app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/photos')
